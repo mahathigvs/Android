e155a4fac9cdfb519d500f2b219767ce16f26559
mahathigvs <mahathi.gottimukkala@gmail.com>
2017-12-10 18:18:47 +0000

    Second Commit

--- justjava/src/main/java/com/example/justjava/MainActivity.java
+++ justjava/src/main/java/com/example/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* just a comment*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
